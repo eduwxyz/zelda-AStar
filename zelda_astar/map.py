94ea6d902a618dd9bcb37248ba9348fdaac6b11c
eduardo machado <eduardoshy1@gmail.com>
2023-04-18 16:13:43 -0300

    add: cria arquivo que irá criar o mapa

--- zelda_astar/map.py
+++ zelda_astar/map.py
@@ -1,7 +1,8 @@
-import pygame
 import math
 from queue import PriorityQueue
+from typing import List, Tuple
 
+import pygame
 
 WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
@@ -20,240 +21,131 @@ TURQUOISE = (64, 224, 208)
 
 
 class Spot:
-    def __init__(self, row, col, width, total_rows):
+    """
+    A class to represent a spot on a grid.
+    """
+
+    def __init__(self, row: int, col: int, width: int, total_rows: int):
+        """
+        Initialize the Spot class.
+
+        Args:
+            row (int): The row index of the spot.
+            col (int): The column index of the spot.
+            width (int): The width of the spot.
+            total_rows (int): The total number of rows in the grid.
+        """
         self.row = row
         self.col = col
         self.x = row * width
         self.y = col * width
-        self.color = WHITE
+        self.color = (255, 255, 255)  # Define a cor como um atributo da classe.
         self.neighbors = []
         self.width = width
         self.total_rows = total_rows
 
-    def get_pos(self):
+    def get_pos(self) -> Tuple[int, int]:
+        """
+        Get the position of the spot.
+
+        Returns:
+            Tuple[int, int]: The row and column index of the spot.
+        """
         return self.row, self.col
 
-    def is_closed(self):
-        return self.color == RED
+    def draw(self, win: pygame.Surface) -> None:
+        """
+        Draw the spot on the given window.
 
-    def is_open(self):
-        return self.color == GREEN
-
-    def is_barrier(self):
-        return self.color == BLACK
-
-    def is_start(self):
-        return self.color == ORANGE
-
-    def is_end(self):
-        return self.color == TURQUOISE
-
-    def reset(self):
-        self.color = WHITE
-
-    def make_closed(self):
-        self.color = RED
-
-    def make_start(self):
-        self.color = ORANGE
-
-    def make_open(self):
-        self.color = GREEN
-
-    def make_barrier(self):
-        self.color = BLACK
-
-    def make_end(self):
-        self.color = TURQUOISE
-
-    def make_path(self):
-        self.color = PURPLE
-
-    def draw(self, win):
+        Args:
+            win (pygame.Surface): The window to draw on.
+        """
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
 
-    def update_neighbors(self, grid):
-        self.neighbors = []
-        # DOWN
-        if (
-            self.row < self.total_rows - 1
-            and not grid[self.row + 1][self.col].is_barrier()
-        ):
-            self.neighbors.append(grid[self.row + 1][self.col])
-
-        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():  # UP
-            self.neighbors.append(grid[self.row - 1][self.col])
-
-        # RIGHT
-        if (
-            self.col < self.total_rows - 1
-            and not grid[self.row][self.col + 1].is_barrier()
-        ):
-            self.neighbors.append(grid[self.row][self.col + 1])
-
-        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier():  # LEFT
-            self.neighbors.append(grid[self.row][self.col - 1])
-
-    def __lt__(self, other):
-        return False
 
 
-# Heuristica da distancia de manhattan
-def h(p1, p2):
-    x1, y1 = p1
-    x2, y2 = p2
-    return abs(x1 - x2) + abs(y1 - y2)
+def make_grid(rows: int, width: int) -> List[List[Spot]]:
+    """
+    Create a grid of spots.
 
+    Args:
+        rows (int): The number of rows in the grid.
+        width (int): The width of the grid.
 
-def reconstruct_path(came_from, current, draw):
-    while current in came_from:
-        current = came_from[current]
-        current.make_path()
-        draw()
-
-
-def algorithm(draw, grid, start, end):
-    count = 0
-    open_set = PriorityQueue()
-    open_set.put((0, count, start))
-    came_from = {}
-    g_score = {spot: float("inf") for row in grid for spot in row}
-    g_score[start] = 0
-    f_score = {spot: float("inf") for row in grid for spot in row}
-    f_score[start] = h(start.get_pos(), end.get_pos())
-
-    open_set_hash = {start}
-
-    while not open_set.empty():
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-
-        current = open_set.get()[2]
-        open_set_hash.remove(current)
-
-        if current == end:
-            reconstruct_path(came_from, end, draw)
-            return True
-
-        for neighbor in current.neighbors:
-            temp_g_score = g_score[current] + 1
-
-            if temp_g_score < g_score[neighbor]:
-                came_from[neighbor] = current
-                g_score[neighbor] = temp_g_score
-                f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
-                if neighbor not in open_set_hash:
-                    count += 1
-                    open_set.put((f_score[neighbor], count, neighbor))
-                    open_set_hash.add(neighbor)
-                    neighbor.make_open()
-
-        draw()
-
-        if current != start:
-            current.make_closed()
-
-    return False
-
-
-def make_grid(rows, width):
+    Returns:
+        List[List[Spot]]: The grid of spots.
+    """
     grid = []
     gap = width // rows
+
+    # Validar argumentos
+    if rows <= 0 or width <= 0:
+        raise ValueError("rows and width must be positive integers")
+
+    def create_spot(i: int, j: int, gap: int, rows: int) -> Spot:
+        """
+        Create a Spot object.
+
+        Args:
+            i (int): The row index of the spot.
+            j (int): The column index of the spot.
+            gap (int): The width of the spot.
+            rows (int): The total number of rows in the grid.
+
+        Returns:
+            Spot: The created spot.
+        """
+        return Spot(i, j, gap, rows)
+
     for i in range(rows):
         grid.append([])
         for j in range(rows):
-            spot = Spot(i, j, gap, rows)
+            spot = create_spot(i, j, gap, rows)
             grid[i].append(spot)
 
     return grid
 
 
-def draw_grid(win, rows, width):
+def draw_grid(win, rows: int, width: int) -> None:
+    """
+    Draw the grid lines on the window.
+
+    Args:
+        win (pygame.Surface): The window to draw on.
+        rows (int): The number of rows in the grid.
+        width (int): The width of the grid.
+
+    Returns:
+        None
+    """
     gap = width // rows
+
     for i in range(rows):
         pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))
         for j in range(rows):
             pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
 
 
-def draw(win, grid, rows, width):
+def draw(win, grid: List[List[Spot]], rows: int, width: int) -> None:
+    """
+    Draw the grid and the spots on the window.
+
+    Args:
+        win (pygame.Surface): The window to draw on.
+        grid (List[List[Spot]]): The grid of spots.
+        rows (int): The number of rows in the grid.
+        width (int): The width of the grid.
+
+    Returns:
+        None
+    """
     win.fill(WHITE)
 
     for row in grid:
         for spot in row:
             spot.draw(win)
 
-    draw_grid(win, rows, width)
+    draw_grid(win, rows, width)  # Chamar a função para desenhar a grade
     pygame.display.update()
 
 
-def get_clicked_pos(pos: tuple, rows: int, width: int):
-    gap = width // rows
-    y, x = pos
-
-    row = y // gap
-    col = x // gap
-
-    return row, col
-
-
-def main(win: pygame.display, width: int):
-    ROWS = 42
-    grid = make_grid(ROWS, width)
-    start = None
-    end = None
-    run = True
-    started = False
-
-    while run:
-        draw(win, grid, ROWS, width)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-
-            if started:
-                continue
-
-            if pygame.mouse.get_pressed()[0]:  # esquerda
-                pos = pygame.mouse.get_pos()
-                row, col = get_clicked_pos(pos, ROWS, width)
-                spot = grid[row][col]
-                if not start and spot != end:
-                    start = spot
-                    start.make_start()
-                elif not end and spot != start:
-                    end = spot
-                    end.make_end()
-                elif spot != end and spot != start:
-                    spot.make_barrier()
-
-            elif pygame.mouse.get_pressed()[2]:  # direita
-                pos = pygame.mouse.get_pos()
-                row, col = get_clicked_pos(pos, ROWS, width)
-                spot = grid[row][col]
-                spot.reset()
-
-                if spot == start:
-                    start = None
-
-                elif spot == end:
-                    end = None
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and start and end:
-                    for row in grid:
-                        for spot in row:
-                            spot.update_neighbors(grid)
-
-                    algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
-
-                if event.key == pygame.K_c:
-                    start = None
-                    end = None
-                    grid = make_grid(ROWS, width)
-
-    pygame.quit()
-
-
-main(WIN, WIDTH)
