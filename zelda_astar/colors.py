d2bdebe9521adb24e0b8429f7525e25868be84d6
eduardo machado <eduardoshy1@gmail.com>
2023-04-24 17:02:30 -0300

    docs: Documenta arquivo colors utilizando como base as boas práticas do python

--- zelda_astar/colors.py
+++ zelda_astar/colors.py
@@ -2,6 +2,12 @@ from typing import List, Tuple
 
 
 def colors() -> List[Tuple[int, int, int]]:
+    """
+    Retorna uma lista de tuplas contendo as cores para cada tipo de célula do grid.
+
+    Returns:
+        List[Tuple[int, int, int]]: Lista de tuplas contendo as cores para cada tipo de célula do grid.
+    """
     colors = {
         "G": {"color": (147, 208, 77), "cost": 10},  # LIGHT_GREEN
         "S": {"color": (195, 187, 147), "cost": 20},  # LIGHT_BROWN
