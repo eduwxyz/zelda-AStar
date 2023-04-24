734c44564077ddedbf30904d355d236aae4edc26
eduardo machado <eduardoshy1@gmail.com>
2023-04-24 17:01:14 -0300

    docs: Documenta arquivo algorithms utilizando como base as boas práticas do python

--- zelda_astar/algorithms.py
+++ zelda_astar/algorithms.py
@@ -1,37 +1,102 @@
 from queue import PriorityQueue
+from time import sleep
 
 import pygame
+from spot import Spot
 from visualization import draw
 
 SIZE = 672
 
-def h(start, end):  # Heurística
+from typing import Any, Callable, Dict, List, Tuple
+
+
+def h(start: Tuple[int, int], end: Tuple[int, int]) -> int:
+    """
+    Heurística para encontrar a distância de Manhattan entre dois pontos.
+
+    Args:
+        start: Uma tupla contendo as coordenadas x e y do ponto de início.
+        end: Uma tupla contendo as coordenadas x e y do ponto de destino.
+
+    Returns:
+        A distância de Manhattan entre os pontos de início e destino, calculada como a soma das diferenças absolutas
+        das coordenadas x e y.
+
+    Raises:
+        TypeError: Se start ou end não forem tuplas contendo duas coordenadas numéricas inteiras.
+    """
+    if not isinstance(start, tuple) or len(start) != 2 or not isinstance(start[0], int) or not isinstance(start[1], int):
+        raise TypeError("start deve ser uma tupla contendo duas coordenadas numéricas inteiras.")
+    if not isinstance(end, tuple) or len(end) != 2 or not isinstance(end[0], int) or not isinstance(end[1], int):
+        raise TypeError("end deve ser uma tupla contendo duas coordenadas numéricas inteiras.")
+    
     x1, y1 = start
     x2, y2 = end
-    return abs(x1 + x2) + abs(y1 - y2)
+    
+    return abs(x1 - x2) + abs(y1 - y2)
 
 
-def reconstruct_path(came_from, current, draw, total=0):
+
+def reconstruct_path(came_from: Dict[Any, Any], current: Any, draw: Callable, total: int = 0) -> int:
+    """
+    Reconstrói o caminho percorrido de um ponto de destino até o ponto de partida a partir de um dicionário de
+    pontos adjacentes.
+
+    Args:
+        came_from: Um dicionário contendo como chaves os pontos visitados e como valores os pontos adjacentes visitados anteriormente durante a busca.
+        current: O ponto de destino do qual se deseja reconstruir o caminho percorrido.
+        draw: Uma função responsável por desenhar os pontos e caminhos percorridos.
+        total: A soma do custo de cada passo do caminho reconstruído (default: 0).
+
+    Returns:
+        A soma do custo de cada passo do caminho reconstruído.
+
+    Raises:
+        TypeError: Se came_from não for um dicionário, current não for um ponto ou draw não for uma função.
+    """
+    if not isinstance(came_from, dict):
+        raise TypeError("came_from deve ser um dicionário.")
+    if not callable(draw):
+        raise TypeError("draw deve ser uma função.")
+    # Verifica se o tipo do argumento current é compatível com a chave de came_from.
+    if current not in came_from:
+        raise TypeError("current não é um ponto válido para o dicionário came_from.")
+    
     list_path = [current]
     while current in came_from:
-        # print(current.cost)
         total = total + current.cost
         print(f"+ {current.cost}")
         current = came_from[current]
         list_path.append(current)
         current.make_path()
-        #sleep(1)
+        #sleep(0.25)
         draw()
 
-    # print(TOTAL)
     return total
 
 
-def best_caminho(WIN, grid):
+def best_caminho(WIN: pygame.Surface, grid: List[List[Tuple[int, int, int]]]) -> Tuple[int, int, int]:
+    """
+    Encontra o melhor caminho a partir de três destinos pré-definidos.
+
+    Args:
+        WIN: Uma superfície do Pygame na qual o caminho será desenhado.
+        grid: Uma matriz que representa o grid do jogo.
+
+    Returns:
+        As coordenadas do destino mais próximo.
+
+    Raises:
+        TypeError: Se WIN não for uma superfície do Pygame ou se grid não for uma matriz.
+    """
+    if not isinstance(WIN, pygame.Surface):
+        raise TypeError("WIN deve ser uma superfície do Pygame.")
+    if not isinstance(grid, list) or not all(isinstance(row, list) and all(isinstance(cell, tuple) and len(cell) == 3 for cell in row) for row in grid):
+        raise TypeError("grid deve ser uma matriz de tuplas com três elementos (cores RGB).")
+        
     lista_dungers = [grid[6][3], grid[2][18], grid[25][2]]
     best_cam = []
     for i in range(3):
-        # breakpoint()
         total = algorithm(
             lambda: draw(WIN, grid, SIZE, 42),
             grid,
@@ -43,7 +108,6 @@ def best_caminho(WIN, grid):
 
     shortest_distance = float("inf")
     for dunger, cost in best_cam:
-        # breakpoint()
         if cost < shortest_distance:
             shortest_distance = cost
             best_dunger = dunger
@@ -51,7 +115,26 @@ def best_caminho(WIN, grid):
     return best_dunger
 
 
-def algorithm(draw, grid, start, end, flag=False):
+def algorithm(draw: Callable, grid: List[List[Spot]], start: Spot, end: Spot, flag: bool = False) -> int:
+    """
+    Implementa o algoritmo A* para encontrar o caminho mais curto em uma grade entre um nó de início e um nó
+    final.
+
+    Argumentos:
+    - draw: uma função que desenha a grade e o caminho encontrado (usando pygame)
+    - grid: uma matriz 2D de objetos Spot representando a grade
+    - start: o nó de início (um objeto Spot)
+    - end: o nó final (um objeto Spot)
+    - flag (opcional): uma flag que indica se a função deve retornar apenas o custo total do caminho encontrado, sem
+    desenhar a grade e o caminho
+
+    Retorna:
+    - Se flag=False, retorna o custo total do caminho encontrado e desenha a grade e o caminho encontrado usando a função
+    draw().
+    - Se flag=True, retorna apenas o custo total do caminho encontrado.
+    """
+    
+    
     count = 0
     came_from = {}
 
